6191997832e63ca027c9eff17fb4f03238320341
MithuBitz <mithubortamuly@gmail.com>
2020-09-28 14:36:39 +0530

    Navigation with DetailFragment.kt...

--- app/src/main/java/com/example/managedata/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/managedata/ui/main/MainFragment.kt
@@ -1,21 +1,28 @@
 package com.example.managedata.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.managedata.LOG_TAG
 import com.example.managedata.R
+import com.example.managedata.data.Monster
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(),
+    MainRecyclerAdapter.MonsterItemListener {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var recyclerView: RecyclerView
     private lateinit var swipeLayout: SwipeRefreshLayout
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +31,11 @@ class MainFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.main_fragment, container, false)
         recyclerView = view.findViewById(R.id.recyclerView)
+        navController = Navigation.findNavController(
+            requireActivity(), R.id.nav_host
+        )
+
+
         swipeLayout = view.findViewById(R.id.swipeLayout)
         swipeLayout.setOnRefreshListener {
             viewModel.refreshData()
@@ -32,13 +44,18 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.monsterData.observe(viewLifecycleOwner, Observer
         {
-            val adapter = MainRecyclerAdapter(requireContext(), it)
+            val adapter = MainRecyclerAdapter(requireContext(), it, this)
             recyclerView.adapter = adapter
             swipeLayout.isRefreshing = false
         })
 
         return view
 
+    }
+
+    override fun onMonsterItemClick(monster: Monster) {
+        Log.i(LOG_TAG, "Selected monster: ${monster.monsterName}")
+        navController.navigate(R.id.action_nav_detail)
     }
 
 }
--- app/src/main/java/com/example/managedata/ui/main/MainRecyclerAdapter.kt
+++ app/src/main/java/com/example/managedata/ui/main/MainRecyclerAdapter.kt
@@ -13,17 +13,10 @@ import com.example.managedata.R
 import com.example.managedata.data.Monster
 import kotlinx.android.synthetic.main.monster_grid_item.view.*
 
-class MainRecyclerAdapter(val context: Context, val monster: List<Monster>) :
+class MainRecyclerAdapter(val context: Context, val monster: List<Monster>, val itemListener: MonsterItemListener) :
     RecyclerView.Adapter<MainRecyclerAdapter.ViewHolder>()
 {
-    inner class ViewHolder(itemView: View) :
-            RecyclerView.ViewHolder(itemView) {
 
-        //Deceleration of the reference view of the recyclerview
-        val nameText = itemView.findViewById<TextView>(R.id.nameText)
-        val monsterImage = itemView.findViewById<ImageView>(R.id.monsterImage)
-        val ratingBar = itemView.findViewById<RatingBar>(R.id.ratingBar)
-    }
 
     override fun getItemCount() = monster.size
 
@@ -44,7 +37,23 @@ class MainRecyclerAdapter(val context: Context, val monster: List<Monster>) :
             Glide.with(context)
                 .load(monster.thumbnailUrl)
                 .into(monsterImage)
+            holder.itemView.setOnClickListener {
+                itemListener.onMonsterItemClick(monster)
+            }
         }
+    }
+
+    inner class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        //Deceleration of the reference view of the recyclerview
+        val nameText = itemView.findViewById<TextView>(R.id.nameText)
+        val monsterImage = itemView.findViewById<ImageView>(R.id.monsterImage)
+        val ratingBar = itemView.findViewById<RatingBar>(R.id.ratingBar)
+    }
+
+    interface MonsterItemListener {
+        fun onMonsterItemClick(monster: Monster)
     }
 
 
